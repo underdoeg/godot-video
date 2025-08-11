4e3b5abdc1964d39665d5fe9577ae924b5c15aa7
Philip Whitfield <philip@undef.ch>
2025-08-11 19:21:52 +0200

    animate one video to test performance

--- demo/example.gd
+++ demo/example.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Control
 #@onready var av_video:AvVideo = $video
 
 @onready var dynamic_video:GAVStream = GAVStream.new()
@@ -6,8 +6,16 @@ extends Node
 func _ready() -> void:
 	dynamic_video.file = "res://input.mp4"
 #
-	$video_dynamic.stream = dynamic_video
-	$video_dynamic.play()
+	%video_dynamic.stream = dynamic_video
+	%video_dynamic.play()
 	
-	$video_tex.texture = $video.get_video_texture()
-	$video_tex2.texture = $video.get_video_texture()
+	%video_tex.texture = %video.get_video_texture()
+	%video_tex2.texture = %video.get_video_texture()
+	
+	_on_timer_timeout()
+
+func _on_timer_timeout():
+	$Timer.wait_time = randf_range(3, 12)
+	var random_rect = Rect2(randf_range(-size.x*.5, size.x*.5), randf_range(-size.y*.5, size.y*.5), randf_range(0, size.x), randf_range(0, size.y))
+	create_tween().tween_property(%video_tex2, "position", random_rect.position, $Timer.wait_time)
+	create_tween().tween_property(%video_tex2, "size", random_rect.size, $Timer.wait_time)
