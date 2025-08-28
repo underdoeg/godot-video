30efbb6cdb879b9b21bd5b7bc6d83f659e8fd8ff
Philip Whitfield <philip@undef.ch>
2025-08-28 16:17:13 +0200

    add cuda decoder

--- src/gav_playback.cpp
+++ src/gav_playback.cpp
@@ -191,8 +191,8 @@ bool GAVPlayback::init_video() {
 		// auto detect v dpau or vaapi /nvidia or intel
 		const auto device_vendor = conversion_rd->get_device_vendor_name().to_lower();
 		UtilityFunctions::print(filename, ": ", "Device vendopr ", device_vendor);
-		if (device_vendor == "nvidia" || device_vendor == "amd") {
-			hw_preferred = AV_HWDEVICE_TYPE_VDPAU;
+		if (device_vendor == "nvidia" ) {
+			hw_preferred = AV_HWDEVICE_TYPE_CUDA;
 		} else {
 			hw_preferred = AV_HWDEVICE_TYPE_VAAPI;
 		}
