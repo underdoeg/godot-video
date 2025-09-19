c3ff2c8f52de7a97962e4299ddfefb970eeeadae
Philip Whitfield <philip@undef.ch>
2025-09-19 13:09:31 +0200

    add demo for single video playback

---
+++ demo/single_video.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _ready():
+	pass
+	#if not OS.has_environment("DESKTOP_SESSION"):
+		#print("no desktop session set, will go fullscren")
+		#DisplayServer.window_set_size(Vector2i(2160, 3840))
+		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		
